83241246c4c450f5f3341d6cf4065ef22db94ded
Ken Dang <k3ndang@gmail.com>
2021-12-19 12:56:49 -0600

    got all post button to server from clients side

--- server/public/scripts/clients.js
+++ server/public/scripts/clients.js
@@ -4,8 +4,10 @@ function onReady() {
     console.log('on ready');
 
     
-    $('#add').on('click', add);
-    $('#equal').on('click', doMath);
+    $('#addBtn').on('click', add);
+    $('#subtractBtnsubtractBtn').on('click', subtract);
+    $('#multipleBtn').on('click', multiple);
+    $('#divideBtn').on('click', divide);
     $('#equal').on('click', doMath);
     $('#clear').on('click', clearAll);
 }
@@ -32,17 +34,62 @@ function doMath(event) {
 
 }
 
-// sending to server
+// sending add buton to server
 function add(){
     $.ajax({
         method: 'POST',
-        url:    'calculation',
+        url:    '/calculation',
         data:   $('#addBtn').data()
     })
     console.log($('#addBtn'.data()));
-}
+} //  end of sending add button to server
 
+// sending subtract button to server
+function subtract(){
+    $.ajax({
+        method: 'POST',
+        url:    '/calculation',
+        data:   $('#subtractBtn').data()
+    })
+    console.log($('#subtractBtn'.data()));
+} //  end of sending subtract button to server
+
+// sending multiple button to server
+function multiple(){
+    $.ajax({
+        method: 'POST',
+        url:    '/calculation',
+        data:   $('#multipleBtn').data()
+    })
+    console.log($('#multipleBtn'.data()));
+} //  end of sending multiple button to server
+
+// sending divide button to server
+function divide(){
+    $.ajax({
+        method: 'POST',
+        url:    '/calculation',
+        data:   $('#divideBtn').data()
+    })
+    console.log($('#divideBtn'.data()));
+} //  end of sending divide button to server
+
+
+// sending equal button to server
+function euqal() {
+    $ajax({
+        method: 'POST',
+        url:    '/calculation',
+        data:   $('#equalBtn').data()
+    })
+} // end of sending equal button 
+
+// sending clear button to server
 function clearAll(){
-
-}
+    $ajax({
+        method: 'POST',
+        url:    '/calculation',
+        data:   $('#clearBtn').data()
+    })
+} // end of sending clear button
 
